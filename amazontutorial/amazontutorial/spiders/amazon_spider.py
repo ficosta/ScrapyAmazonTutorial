4400d3f456b2507caa84b7502a6891e8202b0035
Felipe Costa <felipeiasi@gmail.com>
2019-08-21 23:24:53 -0300

    Spider Amazon

--- amazontutorial/amazontutorial/spiders/amazon_spider.py
+++ amazontutorial/amazontutorial/spiders/amazon_spider.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from ..items import AmazontutorialItem
 
 class AmazonSpiderSpider(scrapy.Spider):
-    name = 'amazon_spider'
-    allowed_domains = ['amazon.com']
-    start_urls = ['http://amazon.com/']
+    name = 'amazon'
+    start_urls = [
+        'https://www.amazon.com/s?k=a+culpa'
+    ]
 
     def parse(self, response):
-        pass
+        items = AmazontutorialItem()
+
+        product_name = response.css('.a-color-base.a-text-normal::text').extract()
+        product_author = response.css('.a-color-secondary .a-size-base+ .a-size-base').css('::text').extract()
+        product_price = response.css('.a-spacing-top-small .a-price-fraction , .a-spacing-top-small .a-price-whole').css('::text').extract()
+        product_imagelink = response.css('.s-image::attr(src)').extract()
+
+        items['product_name'] = product_name
+        items['product_author'] = product_author
+        items['product_price'] = product_price
+        items['product_imagelink'] = product_imagelink
+
+        yield items
+
